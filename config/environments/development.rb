b973c0bd3bfab81dd44fa04f50a0c27a6d0c4abd
Aaron Bali <bali.aaron@gmail.com>
2015-05-09 18:08:56 +0100

    continued CSS developemnt todo: fix fonts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,7 @@ Rails.application.configure do
 
   #needed for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Expands the lines which load the assets
+  config.assets.debug = false #true
 end
