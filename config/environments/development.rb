7cef214a2bbc90c7c8a549c3130357a80a7a4036
Aaron Bali <bali.aaron@gmail.com>
2015-05-11 10:47:12 +0100

    looking at jquery to determine note delete behaviour

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   #needed for devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'notes.aaronba.li', port: 80 }
 
   # Expands the lines which load the assets
   config.assets.debug = false #true
